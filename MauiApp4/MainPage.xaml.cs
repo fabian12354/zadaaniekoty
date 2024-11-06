1e5495966d9783ddc87079cbac406849aef5bd97
fabian12354 <ut_3784@zsp3zamosc.pl>
2024-11-06 09:22:30 +0100

    a

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiApp4
+{
+    
+        public partial class MainPage : ContentPage
+        {
+
+
+            public MainPage()
+            {
+                InitializeComponent();
+            }
+
+            private void OnCounterClicked(object sender, EventArgs e)
+            {
+                string url = "https://cataas.com/cat";
+                string s = entTekst.Text;
+                if (entTekst.Text != null && s.Length > 0)
+                    url += "/says/" + s;
+
+                imgCat.Source = url;
+            }
+        }
+
+    
+
+}
