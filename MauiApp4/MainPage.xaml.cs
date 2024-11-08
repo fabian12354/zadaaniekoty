1d41e5d202e97fb057a2407ad8be84414677b3e3
fabian12354 <ut_3784@zsp3zamosc.pl>
2024-11-08 13:35:23 +0100

    MainPage.xaml.cs

--- MauiApp4/MainPage.xaml.cs
+++ MauiApp4/MainPage.xaml.cs
@@ -1,26 +1,31 @@
-﻿namespace MauiApp4
+namespace MauiApp4
 {
-    
-        public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
         {
-
-
-            public MainPage()
-            {
-                InitializeComponent();
-            }
-
-            private void OnCounterClicked(object sender, EventArgs e)
-            {
-                string url = "https://cataas.com/cat";
-                string s = entTekst.Text;
-                if (entTekst.Text != null && s.Length > 0)
-                    url += "/says/" + s;
-
-                imgCat.Source = url;
-            }
+            InitializeComponent();
         }
 
-    
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string url = "https://cataas.com/cat";
+            string s = entTekst.Text;
+            if (!string.IsNullOrEmpty(s))
+            {
+                url += "/says/" + s;
+            }
 
+            imgCat.Source = url;
+        }
+
+      
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+        
+            double newSize = e.NewValue;
+            imgCat.HeightRequest = newSize;  
+            imgCat.WidthRequest = newSize;   
+        }
+    }
 }
